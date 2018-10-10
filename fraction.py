b3982ef58549f526c3458eeb563bdf018785e73e
radpac <195882@edu.p.lodz.pl>
2018-10-10 15:21:06 +0200

    Added class with expected functionality

--- fraction.py
+++ fraction.py
@@ -18,15 +18,38 @@ class Fraction(object):
     Fraction class.
     """
 
-    def __init__(self, nom, denom):
-        self.nom = nom
-        self.denom = denom
+    def __init__(self, numerator, denominator):
+        greatestCommonDivisor = gcd(numerator, denominator)
+        self.numerator = int(numerator / gcd(abs(numerator), abs(denominator)))
+        self.denominator = int(denominator / gcd(abs(numerator), abs(denominator)))
+        if self.denominator < 0:
+
+            self.denominator *= -1
+            self.numerator *= -1
+        elif self.denominator == 0:
+
+            raise ValueError
+
+    def value(self):
+        result = (self.numerator) / (self.denominator)
+        return result
+
+    def __add__(self, other):
+        return Fraction(self.numerator * other.denominator + self.denominator * other.numerator,
+                        self.denominator * other.denominator)
+
+    def __sub__(self, other):
+        return Fraction(self.numerator * other.denominator - self.denominator * other.numerator,
+                        self.denominator * other.denominator)
+
+    def __mul__(self, other):
+        return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
+
+    def __truediv__(self, other):
+        return Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
 
     def __str__(self):
-        return f"{self.nom}/{self.denom}"
-
-    def __repr__(self):
-        return f"Fraction({self.nom}, {self.denom})"
+        return "%s/%s" % (self.numerator, self.denominator)
 
     def __eq__(self, other):
-        return self.nom == other.nom and self.denom == other.denom
+        return self.numerator == other.numerator and self.denominator == other.denominator
